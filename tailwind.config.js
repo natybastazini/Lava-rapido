a529e5c185a6c6eebb79444e355074fbd1f70ba5
natybastazini <natybastazini7@gmail.com>
2024-06-04 08:42:52 -0300

    em processo

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        principal: '#0B8185',
+        trocaBotao: '#36544F',
+        trocabotao2: '#403831',
+        input: '#F0EDED',
+        letra: '#403831',
+        borda: '#1F5F61',
+        
+      },
+      fontFamily: {
+        fontDestaque: ['Michroma', 'sans-serif'],
+        font: ['Kanit', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
